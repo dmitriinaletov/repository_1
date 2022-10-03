c72a303936dcacb766310b1e1da4ac65b1b9b18a
dmitriinaletov <dmitriinaletov@gmail.com>
2022-10-03 14:30:59 +0200

    First_commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Header = () => {
+    return <header className='header'>
+        <img src='https://e7.pngegg.com/pngimages/140/849/png-clipart-jumpman-air-jordan-nike-converse-sneakers-nike-logo-basketballschuh-thumbnail.png' />
+    </header>;
+}
+
+export default Header
---
+++ src/components/Profile.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const Profile = () => {
+    return <div className='content'>
+        <div>
+            <img src='https://images.stockx.com/images/Air-Jordan-23-All-Star-Product.jpg?fit=fill&bg=FFFFFF&w=1200&h=857&fm=webp&auto=compress&trim=color&dpr=2&updated_at=1611930726&q=75' />
+        </div>
+        <div>
+            ava + description
+        </div>
+        <div>
+            My posts
+            <div>
+                New post
+            </div>
+            <div>
+                Post 1
+            </div>
+            <div>
+                Post 2
+            </div>
+        </div>
+    </div>;
+}
+
+export default Profile
